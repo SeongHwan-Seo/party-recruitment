329203791ef0c8e15ea7211b437e7d5bf66e156e
seosh5704 <seosh5704@gmail.com>
2022-05-18 21:45:57 +0900

    PXZG-17 using EmailLogin

--- App/IOS/PartyRecruitment/PartyRecruitment/View/LoginView.swift
+++ App/IOS/PartyRecruitment/PartyRecruitment/View/LoginView.swift
@@ -9,74 +9,123 @@ import SwiftUI
 
 struct LoginView: View {
     @StateObject var viewModel = LoginViewModel()
-    
+    @State private var showingSheet = false
     var body: some View {
         VStack(spacing: 7.0) {
-                    Text("이메일")
-                        .frame(maxWidth: .infinity, alignment: .leading)
+            Text("이메일")
+                .frame(maxWidth: .infinity, alignment: .leading)
             TextField("이메일을 입력해 주세요.", text: $viewModel.email)
-                        .frame(height: 40)
-                        .disableAutocorrection(true)
-                        .autocapitalization(.none)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .cornerRadius(9)
-                    Text("비밀번호")
-                        .frame(maxWidth: .infinity, alignment: .leading)
+                .frame(height: 40)
+                .disableAutocorrection(true)
+                .autocapitalization(.none)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .cornerRadius(9)
+            Text("비밀번호")
+                .frame(maxWidth: .infinity, alignment: .leading)
             SecureField("비밀번호를 입력해 주세요.", text: $viewModel.password)
-                        .frame(height: 40)
-                        .disableAutocorrection(true)
-                        .autocapitalization(.none)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .cornerRadius(9)
-                    
-                    Button(action: {
-                        viewModel.withEmailLogin()
-                        
-                    }, label: {
-                        Image(systemName: "mail")
-                            .foregroundColor(.white)
-                        Text("이메일로 계속하기")
-                            .foregroundColor(.white)
-                    })
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.blue)
-                        .cornerRadius(10)
-                        .padding(.top, 10)
-                    
-                    Text("or")
-                        .font(.system(size: 20))
-                        .foregroundColor(.gray)
-                            
-                            
-                    
-                    
+                .frame(height: 40)
+                .disableAutocorrection(true)
+                .autocapitalization(.none)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .cornerRadius(9)
+            
+            Button(action: {
+                viewModel.withEmailLogin()
+                
+            }, label: {
+                Image(systemName: "mail")
+                    .foregroundColor(.white)
+                Text("이메일로 로그인하기")
+                    .foregroundColor(.white)
+            })
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color.blue)
+            .cornerRadius(10)
+            .padding(.top, 10)
+            
+            Text("or")
+                .font(.system(size: 20))
+                .foregroundColor(.gray)
+            
+            
+            
+            
             HStack(spacing: 40){
-                        Button(action: {
-                            
-                        }, label: {
-                            Image("logo_google")
-                                .resizable()
-                                .frame(width: 50, height: 50)
-                        })
-                        .frame(width: 60, height: 60)
-                        .background(Circle().stroke(.gray, lineWidth: 1))
-                        
-                        Button(action: {
-                            
-                        }, label: {
-                            Image("logo_apple")
-                                .resizable()
-                                .frame(width: 50, height: 50)
-                        })
-                        .frame(width: 60, height: 60)
-                        .background(.black)
-                        .clipShape(Circle())
-                    } //HStack
+                Button(action: {
                     
-                } //VStack
-                .padding()
+                }, label: {
+                    Image("logo_google")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                })
+                .frame(width: 60, height: 60)
+                .background(Circle().stroke(.gray, lineWidth: 1))
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image("logo_apple")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                })
+                .frame(width: 60, height: 60)
+                .background(.black)
+                .clipShape(Circle())
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image("logo_kakao")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                })
+                .frame(width: 60, height: 60)
+                .background(.yellow)
+                .clipShape(Circle())
+            } //HStack
+            
+            
+            Text("이메일로 가입하기")
+                .underline()
+                .padding(.top, 50)
+                .foregroundColor(.gray)
+                .onTapGesture {
+                    showingSheet.toggle()
+                }
+                .sheet(isPresented: $showingSheet) {
+                    SheetView()
+                }
+            
+        } //VStack
+        .padding()
+        
+    }
+}
 
+
+//Sheet View
+struct SheetView: View{
+    @StateObject var viewModel = LoginViewModel()
+    var body: some View {
+        VStack(spacing: 7.0) {
+            Text("이메일")
+                .frame(maxWidth: .infinity, alignment: .leading)
+            TextField("이메일을 입력해 주세요.", text: $viewModel.email)
+                .frame(height: 40)
+                .disableAutocorrection(true)
+                .autocapitalization(.none)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .cornerRadius(9)
+            Text("비밀번호")
+                .frame(maxWidth: .infinity, alignment: .leading)
+            SecureField("비밀번호를 입력해 주세요.", text: $viewModel.password)
+                .frame(height: 40)
+                .disableAutocorrection(true)
+                .autocapitalization(.none)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .cornerRadius(9)
+        }
     }
 }
 
--- App/IOS/PartyRecruitment/PartyRecruitment/ViewModel/LoginViewModel.swift
+++ App/IOS/PartyRecruitment/PartyRecruitment/ViewModel/LoginViewModel.swift
@@ -15,16 +15,53 @@ class LoginViewModel: ObservableObject {
     @Published var isValid = false
     
     func withEmailLogin() {
-        //Firebase EmailLogin
+        //Firebase SignInEmailLogin
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+            if authResult != nil {
+                print("로그인 성공!!")
+            } else {
+                print("error: \(error!)")
+                guard let errorCode = AuthErrorCode(rawValue: error!._code) else { return }
+                switch errorCode.rawValue {
+                case 17009:
+                    print("비밀번호를 확인해 주세요.")
+                case 17011:
+                    print("등록된 이메일이 아닙니다.")
+                case 17008:
+                    print("이메일 형식이 아닙니다.")
+                default:
+                    print("고객센터에 문의 하시기 바랍니다.")
+                }
+            }
+            
+        }
+        
+        
+        //print(email, password)
+    }
+    
+    func createUserWithEmail() {
+        //Firebase CreateUser
         Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
             if authResult != nil {
                 print("성공")
             } else {
-                print(error)
+                guard let errorCode = AuthErrorCode(rawValue: error!._code) else { return }
+                switch errorCode.rawValue {
+                case 17007:
+                    print("이미 등록된 이메일 입니다.")
+                case 17008:
+                    print("이메일 형식이 아닙니다.")
+                case 17026:
+                    print("비밀번호는 6자 이상 이여야 합니다.")
+                default:
+                    print("고객센터에 문의 하시기 바랍니다.")
+                }
+                
+                
             }
             
         }
-        //print(email, password)
     }
 }
 
